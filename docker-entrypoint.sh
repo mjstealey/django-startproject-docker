e39518f29ed113c4294b76f3b07132ac5fa20c70
Michael J. Stealey <michael.j.stealey@gmail.com>
2018-05-27 16:11:07 -0400

    refactor structure
    Refactor the project structure to be ready for expansion with file type specification already broken out (apps, plugins, etc). Settings files now in a directory named settings and organized by purpose

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -48,51 +48,16 @@ _generate_run_uwsgi_sh() {
 
 source venv/bin/activate
 python manage.py makemigrations
+python manage.py showmigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
 uwsgi --static-map /static/=static/ \\
-    --static-map /images/=images/ \\
+    --static-map /media/=media/ \\
     --http-auto-chunked \\
     --http-keepalive \\
     uwsgi.ini
 EOF
     chmod +x /code/$PROJECT_NAME/run_uwsgi.sh
-}
-
-### generate secrets.py file ###
-_generate_secrets_py() {
-cat > /code/$PROJECT_NAME/$PROJECT_NAME/secrets.py << EOF
-import os
-
-DEBUG = os.getenv('DEBUG', 'True')
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', '${POSTGRES_DB}'),
-        'USER': os.getenv('POSTGRES_USER', '${POSTGRES_USER}'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', '${POSTGRES_PASSWORD}'),
-        'HOST': os.getenv('POSTGRES_HOST', '${POSTGRES_HOST}'),
-        'PORT': os.getenv('POSTGRES_PORT', '${POSTGRES_PORT}'),
-    }
-}
-EOF
-}
-
-### update settings.py file ###
-_update_settings_py() {
-sed -i '/import os/a from dotenv import load_dotenv\nload_dotenv(\x27'${PROJECT_NAME}'\x2F.env\x27)' /code/$PROJECT_NAME/$PROJECT_NAME/settings.py
-sed -i 's/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = ["*"]/' /code/$PROJECT_NAME/$PROJECT_NAME/settings.py
-    cat >> /code/$PROJECT_NAME/$PROJECT_NAME/settings.py << EOF
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
-
-try:
-    from .secrets import *
-except ImportError:
-    pass
-EOF
 }
 
 ### generate Dockerfile file ###
@@ -101,23 +66,13 @@ _generate_dockerfile() {
 FROM python:3.6
 MAINTAINER Michael J. Stealey <mjstealey@gmail.com>
 
-RUN apt-get update && apt-get install -y \\
-    postgresql-client
-
-RUN mkdir /code/
-COPY . /code/
+RUN apt-get update && apt-get install -y \
+    postgresql-client \
+  && pip install virtualenv \
+  && mkdir /code/
 
 WORKDIR /code
-RUN if [ -d /code/venv ]; then rm -rf /code/venv; fi \\
-    && if [ -d /code/static ]; then rm -rf /code/static; fi \\
-    && if [ -d /code/media ]; then rm -rf /code/media; fi \\
-    && python -m venv venv \\
-    && . venv/bin/activate \\
-    && venv/bin/pip install --upgrade pip \\
-    && venv/bin/pip install -r requirements.txt
-
 VOLUME ["/code"]
-
 ENTRYPOINT ["/code/docker-entrypoint.sh"]
 EOF
 }
@@ -133,6 +88,11 @@ until [ \$(pg_isready -h database -q)\$? -eq 0 ]; do
   sleep 1
 done
 
+virtualenv -p /usr/local/bin/python venv
+source venv/bin/activate
+venv/bin/pip install --upgrade pip
+venv/bin/pip install -r requirements.txt
+
 >&2 echo "Postgres is up - continuing"
 
 ./run_uwsgi.sh
@@ -144,7 +104,7 @@ EOF
 
 ### generate docker-compose.yml file ###
 _generate_docker_compose_yml() {
-cat > /code/$PROJECT_NAME/docker-compose.yml << EOF
+    cat > /code/$PROJECT_NAME/docker-compose.yml << EOF
 version: '3.0'
 services:
 
@@ -173,13 +133,149 @@ services:
       - 8000:8000
       - 8443:443
       - 8080:80
+    volumes:
+      - .:/code
+      - ./static:/code/static
+      - ./media:/code/media
+EOF
+}
+
+# populate settings directory
+_generate_settings() {
+    local SETTINGS_DIR=/code/$PROJECT_NAME/$PROJECT_NAME/settings
+    local SETTINGS_PY=/code/$PROJECT_NAME/$PROJECT_NAME/settings.py
+    mkdir -p $SETTINGS_DIR
+    # generate __init__.py
+    cat > $SETTINGS_DIR/__init__.py << EOF
+from importlib import import_module
+
+from .applications import *
+from .config import *
+from .main import *
+from .logging import *
+from .auth import *
+from .api import *
+from .tasks import *
+EOF
+    # generate api.py
+    touch $SETTINGS_DIR/api.py
+    # generate applications.py
+    cat > $SETTINGS_DIR/applications.py <<EOF
+import os
+
+# Application definition
+
+EOF
+    sed -n -e '/^INSTALLED_APPS/,/^]/p' $SETTINGS_PY >> $SETTINGS_DIR/applications.py
+    cat >> $SETTINGS_DIR/applications.py <<EOF
+
+THIRD_PARTY_APPS = []
+
+INSTALLED_APPS += THIRD_PARTY_APPS
+EOF
+    sed -i -e '/^INSTALLED_APPS/,/^]/d' $SETTINGS_PY
+    sed -i -e '/^# Application definition/d' $SETTINGS_PY
+    # generate auth.py
+    touch $SETTINGS_DIR/auth.py
+    # generate config.py
+    cat > $SETTINGS_DIR/config.py << EOF
+import os
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.getenv('DEBUG', True)
+
+EOF
+    sed -n -e '/^# Database/,/^# https:\/\/docs.djangoproject.com/p' $SETTINGS_PY >> $SETTINGS_DIR/config.py
+    cat >> $SETTINGS_DIR/config.py << EOF
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', '${POSTGRES_DB}'),
+        'USER': os.getenv('POSTGRES_USER', '${POSTGRES_USER}'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', '${POSTGRES_PASSWORD}'),
+        'HOST': os.getenv('POSTGRES_HOST', '${POSTGRES_HOST}'),
+        'PORT': os.getenv('POSTGRES_PORT', '${POSTGRES_PORT}'),
+    }
+}
+EOF
+    sed -i -e '/^# Database/,/^# https:\/\/docs.djangoproject.com/d' $SETTINGS_PY
+    sed -i -e '/^DATABASES/,/^}/d' $SETTINGS_PY
+    # generate logging.py
+    cat > $SETTINGS_DIR/logging.py << EOF
+import os
+
+# Default Django logging is WARNINGS+ to console
+# so visible via docker-compose logs uwsgi
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'WARNING'),
+        },
+    },
+}
+EOF
+    # generate secrets.py
+    cat > $SETTINGS_DIR/dummy_secrets.py << EOF
+# This file, dummy_secrets, provides an example of how to configure
+# sregistry with your authentication secrets. Copy it to secrets.py and
+# configure the settings you need.
+
+# Secret Key
+# You must uncomment, and set SECRET_KEY to a secure random value
+# e.g. https://djskgen.herokuapp.com/
+
+#SECRET_KEY = 'xxxxxxxxxxxxxxxxxx'
+
+EOF
+    cat > $SETTINGS_DIR/secrets.py << EOF
+# This file, dummy_secrets, provides an example of how to configure
+# sregistry with your authentication secrets. Copy it to secrets.py and
+# configure the settings you need.
+
+# Secret Key
+# You must uncomment, and set SECRET_KEY to a secure random value
+# e.g. https://djskgen.herokuapp.com/
+
+EOF
+    sed -n -e '0,/^# SECURITY WARNING/{//p;}' $SETTINGS_PY >> $SETTINGS_DIR/secrets.py
+    sed -n -e '/^SECRET_KEY/p' $SETTINGS_PY >> $SETTINGS_DIR/secrets.py
+    sed -i -e '0,/^# SECURITY WARNING/{//d;}' $SETTINGS_PY
+    sed -i -e '/^SECRET_KEY/d' $SETTINGS_PY
+    # generate tasks.py
+    touch $SETTINGS_DIR/tasks.py
+    # generate main.py
+    mv $SETTINGS_PY $SETTINGS_DIR/main.py
+    sed -i '/import os/a from dotenv import load_dotenv\nload_dotenv(\x27'${PROJECT_NAME}'\x2F.env\x27)' $SETTINGS_DIR/main.py
+     sed -i '/^BASE_DIR/c\BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))' $SETTINGS_DIR/main.py
+    sed -i '/^# SECURITY WARNING/d' $SETTINGS_DIR/main.py
+    sed -i '/^DEBUG/d' $SETTINGS_DIR/main.py
+    sed -i 's/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = ["*"]/' $SETTINGS_DIR/main.py
+    cat >> $SETTINGS_DIR/main.py << EOF
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+try:
+    from .secrets import *
+except ImportError:
+    pass
 EOF
 }
 
 ### main ###
 cp /requirements.txt /code/requirements.txt
 
-python -m venv /venv
+pip install virtualenv
+virtualenv -p /usr/local/bin/python /venv
 source /venv/bin/activate
 /venv/bin/pip install --upgrade pip
 /venv/bin/pip install -r requirements.txt
@@ -190,8 +286,9 @@ source /venv/bin/activate
 _generate_env
 source /code/$PROJECT_NAME/$PROJECT_NAME/.env
 
-_update_settings_py
-_generate_secrets_py
+mkdir -p /code/$PROJECT_NAME/apps /code/$PROJECT_NAME/plugins
+touch /code/$PROJECT_NAME/plugins/__init__.py
+_generate_settings
 _generate_uwsgi_ini
 _generate_run_uwsgi_sh
 _generate_dockerfile
